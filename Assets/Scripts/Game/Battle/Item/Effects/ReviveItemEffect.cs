dfac22f4c2907c24c3b2d057b014d0d9be0bcea8
davidFernandez99 <davidfernandezfernandez.1999@gmail.com>
2026-05-24 13:14:58 +0200

    #56 show dead unit on turn order

--- Assets/Scripts/Game/Battle/Item/Effects/ReviveItemEffect.cs
+++ Assets/Scripts/Game/Battle/Item/Effects/ReviveItemEffect.cs
@@ -8,14 +8,15 @@ namespace Game.Battle.Item.Effects {
 
     [CreateAssetMenu(menuName = "Item/Effects/Revive Item effect")]
     public class ReviveItemEffect : ItemEffect {
-        public int amount;
+        public float amount;
 
         public override bool CanApply(UnitObject target) => target.Unit.IsDead();
 
         public override IEnumerator Apply(UnitObject user, GridPosition target, BattleMapManager battleMapManager) {
             UnitObject targetUnit = battleMapManager.GetUnit(target);
             Unit unit = targetUnit.Unit;
-            float heal = unit.AddStat(StatType.Hp, this.amount);
+            float maxHp = unit.GetMaxStat(StatType.Hp);
+            float heal = unit.AddStat(StatType.Hp, Mathf.Max(1, maxHp * this.amount));
             yield return targetUnit.PlayRevive((int)heal);
         }
     }
--- Assets/Scripts/Game/Battle/UI/OrderUnitUI.cs
+++ Assets/Scripts/Game/Battle/UI/OrderUnitUI.cs
@@ -8,8 +8,19 @@ namespace Game.Battle.UI {
         [SerializeField] private Image highlightImage;
         [SerializeField] private Image unitImage;
         [SerializeField] private Image unitBorderImage;
+        [SerializeField] private Image deadUnitImage;
+
+        private UnitObject _unit;
+
+        private void Update() {
+            if (this.deadUnitImage == null || this._unit == null) {
+                return;
+            }
+            this.deadUnitImage.gameObject.SetActive(this._unit.Unit.IsDead());
+        }
 
         public void SetUnit(UnitObject unit) {
+            this._unit = unit;
             this.unitImage.sprite = unit.GetSprite();
             if (this.unitBorderImage != null) {
                 this.unitBorderImage.color = unit.Team.GetBattleTeam().GetColor();
