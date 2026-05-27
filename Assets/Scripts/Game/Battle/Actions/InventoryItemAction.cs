1dd7b2bcada6790203633af3c9825097b1953d3f
davidFernandez99 <davidfernandezfernandez.1999@gmail.com>
2026-05-28 00:13:44 +0200

    #55 add boss animations

--- Assets/Scripts/Game/Battle/Actions/InventoryItemAction.cs
+++ Assets/Scripts/Game/Battle/Actions/InventoryItemAction.cs
@@ -20,7 +20,7 @@ namespace Game.Battle.Actions {
 
         public string GetName() => this._item.GetName();
 
-        public string GetActionName() => ActionType.Item.GetActionId();
+        public string GetActionName() =>  this._item.GetId();
 
         public int GetApCost() => 0;
 
